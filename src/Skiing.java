70f8fbb32c25019b0f46b9018bb593ef561fe811
zhzero <545033805@qq.com>
2020-02-01 16:35:34 +0800

    2.1

--- src/Skiing.java
+++ src/Skiing.java
@@ -4,12 +4,12 @@ import java.util.*;
  * Created by Z_HAO on 2020/2/1
  * 一个样例 MLE，想不到怎么优化
  */
-class Node {
+class CoordinatesAndHeight {
     int x;
     int y;
     int height;
 
-    public Node(int a , int b , int c) {
+    public CoordinatesAndHeight(int a , int b , int c) {
         x = a;
         y = b;
         height = c;
@@ -22,14 +22,14 @@ public class Skiing {
 
     static int bfs(int i , int j , int [][]g , int [][]ans) {
         int ret = 0;
-        Queue<Node>que = new ArrayDeque<>();
-        que.add(new Node(i , j , g[i][j]));
+        Queue<CoordinatesAndHeight>que = new ArrayDeque<>();
+        que.add(new CoordinatesAndHeight(i , j , g[i][j]));
         while(!que.isEmpty()) {
-            Node pair = que.element();
+            CoordinatesAndHeight pair = que.element();
             que.remove();
             int x = pair.x , y = pair.y;
             if(g[x + 1][y] < g[x][y]) {
-                que.add(new Node(x + 1 , y , g[x + 1][y]));
+                que.add(new CoordinatesAndHeight(x + 1 , y , g[x + 1][y]));
                 if(ans[x][y] + 1 < ans[x + 1][y]) {
                     continue;
                 }
@@ -39,7 +39,7 @@ public class Skiing {
                 }
             }
             if(g[x - 1][y] < g[x][y]) {
-                que.add(new Node(x - 1 , y , g[x - 1][y]));
+                que.add(new CoordinatesAndHeight(x - 1 , y , g[x - 1][y]));
                 if(ans[x][y] + 1 < ans[x - 1][y]) {
                     continue;
                 }
@@ -49,7 +49,7 @@ public class Skiing {
                 }
             }
             if(g[x][y + 1] < g[x][y]) {
-                que.add(new Node(x , y + 1 , g[x][y + 1]));
+                que.add(new CoordinatesAndHeight(x , y + 1 , g[x][y + 1]));
                 if(ans[x][y] + 1 < ans[x][y + 1]) {
                     continue;
                 }
@@ -59,7 +59,7 @@ public class Skiing {
                 }
             }
             if(g[x][y - 1] < g[x][y]) {
-                que.add(new Node(x , y - 1 , g[x][y - 1]));
+                que.add(new CoordinatesAndHeight(x , y - 1 , g[x][y - 1]));
                 if(ans[x][y] + 1 < ans[x][y - 1]) {
                     continue;
                 }
@@ -72,16 +72,16 @@ public class Skiing {
         return ret + 1;
     }
 
-    static Comparator<Node> cmp = new Comparator<Node>() {
+    static Comparator<CoordinatesAndHeight> cmp = new Comparator<CoordinatesAndHeight>() {
         @Override
-        public int compare(Node node, Node t1) {
+        public int compare(CoordinatesAndHeight node, CoordinatesAndHeight t1) {
             return t1.height - node.height;
         }
     };
 
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Node> arr = new ArrayList<>();
+        ArrayList<CoordinatesAndHeight> arr = new ArrayList<>();
         row = sc.nextInt();
         line = sc.nextInt();
         int [][]g = new int [row + 2][line + 2];
@@ -94,14 +94,14 @@ public class Skiing {
         for(int i = 1;i <= row;i ++) {
             for(int j = 1;j <= line;j ++) {
                 g[i][j] = sc.nextInt();
-                Node temp = new Node(i , j , g[i][j]);
+                CoordinatesAndHeight temp = new CoordinatesAndHeight(i , j , g[i][j]);
                 arr.add(temp);
             }
         }
         int [][]ans = new int [row + 1][line + 1];
         int ret = 0;
         arr.sort(cmp);
-        for(Node k: arr) {
+        for(CoordinatesAndHeight k: arr) {
             ret = Math.max(ret , bfs(k.x ,k.y , g , ans));
         }
         System.out.println(ret);
